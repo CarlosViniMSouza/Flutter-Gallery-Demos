92d5368f438985378f9776ce2ef8bc2e5bc69122
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-01-10 18:02:26 -0300

    bottom_app_bar.dart -> add

--- autogit.sh
+++ autogit.sh
@@ -1,5 +1,5 @@
 git add -A
-git commit -m "banner.dart -> add"
+git commit -m "bottom_app_bar.dart -> add"
 git push
 git status
 exit
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'app_bar.dart';
+import 'bottom_app_bar.dart';
 
 void main() {
   runApp(const MyApp());
